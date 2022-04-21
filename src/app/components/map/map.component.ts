cc910f8cd131c1ba879dc6c1677b1083a0a417fa
hunglqhe153247 <92884206+hunglqhe153247@users.noreply.github.com>
2022-04-21 10:38:09 +0700

    update map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { circle, latLng, LayerGroup, tileLayer } from 'leaflet';
+import { AppService } from 'src/app/service/app.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors',
-      }),
-    ],
-    zoom: 1,
-    center: latLng([0, 0]),
-  };
+  constructor(private AppService: AppService) {}
+  regions: any = [];
+  options: any;
+  regionCircle: any = [];
+  ngOnInit(): void {
+    this.options = {
+      layers: [
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; OpenStreetMap contributors',
+        }),
+      ],
+      zoom: 1,
+      center: latLng([0, 0]),
+    };
+    this.AppService.getRegions().subscribe((data) => {
+      this.regions = data;
+      for (var val of this.regions) {
+        this.regionCircle.push(val);
+      }
+    });
+    console.log(this.regionCircle);
+    this.options.layers.push(circle([46.95, -122], { radius: 5000 }));
+  }
 }
